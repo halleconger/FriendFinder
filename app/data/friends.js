f7ccdf1706419943012f57c3961641c9ecc435d0
Halle Conger <halle.conger@gmail.com>
2018-05-25 13:38:55 -0500

    partially functional

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+var friendsData = [
+    {
+        name: "Quinn",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Drew",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            1,
+            3,
+            5,
+            2,
+            4,
+            5,
+            5,
+            5,
+            2,
+            3
+        ]
+    },
+    {
+        name: "Jayde",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            5,
+            3,
+            3,
+            2,
+            3,
+            1,
+            5,
+            4,
+            2,
+            2
+        ]
+    },
+    {
+        name: "Conor",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            3,
+            3,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Kendall",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Maxwell",
+        photo: "http://via.placeholder.com/150x150",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }
+]
+
+// EXPORT TO ROUTE SO THAT IT IS ACCESSIBLE
+module.exports = friendsData;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,59 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information the users
+// ===============================================================================
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a survey... this data is then sent to the server...
+// Then the server saves the data to the friendsData array)
+    // Note the code here. Our "server" will respond to users with their best match
+    // req.body is available since we're using the body-parser middleware
+    app.post("/api/friends", function(req, res) {
+        var userData = req.body;
+        var userScore = userData.scores;
+        var bestMatch = {
+            name: "",
+            photo: "",
+            difference: 50
+        }
+        
+        
+        for (var i = 0; i < friendsData.length; i++) {
+            var totalDifference = 0;
+
+            for (var j = 0; j < userScore.length; j++) {
+                totalDifference += Math.abs(parseInt(userScore[i]) - parseInt(friendsData[i].scores[j]));
+
+                if (totalDifference <= bestMatch.difference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.difference = totalDifference;
+                }
+            }
+        }
+
+        friends.push(userData);
+
+        res.json(bestMatch);
+    });
+
+}
